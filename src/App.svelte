79e5139a2975b75dbf8d162eaa27a0ebe010a31d
Daniel <danieljackson104@googlemail.com>
2022-03-01 23:18:38 +0000

    adding deleting tasks to and css styles foer button

--- src/App.svelte
+++ src/App.svelte
@@ -17,10 +17,13 @@
       text: "Many novice writers tend to make a sharp distinction between content and style, thinking that a paper can be strong in one and weak in the other, but focusing on organization shows how content and style converge in deliberative academic writing. Your professors will view even the most elegant prose as rambling and tedious if there isn’t a careful, coherent argument to give the text meaning. Paragraphs are the “stuff ” of academic writing and, thus, worth our attention here. ",
     },
   ]
+    const deletefeedback = (e) => {
+        console.log(e.detail)
+    }
 </script>
 
 <main class="container">
     
-    <FeedbackList {feedback}/>
+    <FeedbackList {feedback} on:delete-feedback={deletefeedback}/>
 
 </main>
