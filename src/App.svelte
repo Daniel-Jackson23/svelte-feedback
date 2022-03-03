1219ce57e93ca8060c4139173b0765ac56870419
Daniel <danieljackson104@googlemail.com>
2022-03-03 20:59:23 +0000

    adding new new compoments for cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
     import FeedbackList from './compoments/FeedbackList.svelte'
+    import FeedbackStats from "./compoments/FeedbackStats.svelte";
+    import FeedbackForm from "./compoments/FeedbackForm.svelte";
   let feedback = [
     {
       id: 1,
@@ -13,17 +15,21 @@
     },
     {
       id: 3,
-      rating: 4,
+      rating: 8,
       text: "Many novice writers tend to make a sharp distinction between content and style, thinking that a paper can be strong in one and weak in the other, but focusing on organization shows how content and style converge in deliberative academic writing. Your professors will view even the most elegant prose as rambling and tedious if there isn’t a careful, coherent argument to give the text meaning. Paragraphs are the “stuff ” of academic writing and, thus, worth our attention here. ",
     },
   ]
-    const deletefeedback = (e) => {
-        console.log(e.detail)
+
+    $: count = feedback.length
+    $: average = feedback.reduce((a, {rating}) => a + rating , 0 ) / feedback.length
+    const deleteFeedback = (e) => {
+        const itemId = e.detail
+        feedback = feedback.filter((item) => item.id != itemId)
     }
 </script>
 
 <main class="container">
-    
-    <FeedbackList {feedback} on:delete-feedback={deletefeedback}/>
-
+    <FeedbackForm />
+    <FeedbackStats {count} {average} />
+    <FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
 </main>
